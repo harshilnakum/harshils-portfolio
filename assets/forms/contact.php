b9a7f75b7c0303285b6bf2b0a35daad37a23ee6a
harshil <nakum.harshil2212@gmail.com>
2022-06-07 01:17:29 +0530

    Initial Commit

---
+++ assets/forms/contact.php
@@ -0,0 +1,21 @@
+<?php
+
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$to = "nakum.harshil2212@gmail.com";
+$headers = "From: ".$email . "\r\n";
+
+if(mail($to,$subject,$message,$headers)){
+    http_response_code(200);
+    echo json_encode("Your message has been sent. Thank you!");
+
+}else{
+    http_response_code(200);
+    echo json_encode(array("msg" => "Something went wrong, Try Again!"));
+
+};
+?>
